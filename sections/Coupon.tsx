88b86e51c6158cd27376f23311221e1535402b04
Felipe Nogueira <felipenogueira.94@gmail.com>
2024-05-15 09:46:30 -0300

    add coupon resources

--- sections/Coupon.tsx
+++ sections/Coupon.tsx
@@ -1,10 +1,44 @@
+import { useSignal } from "@preact/signals";
+import IconCopy from "https://deno.land/x/tabler_icons_tsx@0.0.5/tsx/copy.tsx";
+
 interface Props {
-  /**
-  * @description The description of name.
-  */
-  name?: string;
+  coupon?: string;
 }
 
-export default function Section({ name = "Capy" }: Props) {
-  return <div>Hello {name}</div>
+export default function Section({ coupon = "SAOStore" }: Props) {
+  const copied = useSignal(false);
+
+  const handleCopy = async () => {
+    try {
+      await navigator.clipboard.writeText(coupon);
+      copied.value = true;
+      setTimeout(() => {
+        copied.value = false;
+      }, 2000); // Reseta o estado após 2 segundos
+    } catch (err) {
+      console.error("Failed to copy text: ", err);
+    }
+  };
+
+  return (
+    <div className="flex flex-col p-6 justify-center items-center">
+      <div>
+        <p className="text-2xl">
+          Cupom de desconto
+        </p>
+      </div>
+
+      <div
+        className="flex justify-center items-center p-2 border-2 border-zinc-700 border-solid gap-4 mt-3 rounded cursor-pointer"
+        onClick={handleCopy}
+      >
+        <p className="font-bold text-lg">
+          {coupon}
+        </p>
+        <IconCopy class="w-6 h-6" />
+      </div>
+
+      {copied.value && <p className="text-green-500 mt-2">Copiado!</p>}
+    </div>
+  );
 }
